70f54788ec03ef29a8379f4d5c07e1d78404bcaf
ElCobrador <philippe.audet97@gmail.com>
2020-09-01 12:50:05 -0400

    Project base + Bootstrap installation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MapSelectionComponent } from './map-selection/map-selection.component';
+import { SiteSelectionComponent } from './site-selection/site-selection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapSelectionComponent,
+    SiteSelectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/map-selection/map-selection.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-map-selection',
+  templateUrl: './map-selection.component.html',
+  styleUrls: ['./map-selection.component.css']
+})
+export class MapSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
